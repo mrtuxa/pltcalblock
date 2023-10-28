5063d043323fcbf9f2502e9cd7aa5f691e70f47e
mrtuxa <mrtuxa@leipzig.freifunk.net>
2023-10-28 12:07:20 +0200

    removed node_modules

--- flake.nix
+++ flake.nix
@@ -8,13 +8,9 @@
     nixpkgs,
   }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
-    fooScript = pkgs.writeScriptBin "foo.sh" ''
-      #!/bin/sh
-      echo $FOO
-    '';
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
-      name = "My-project build environment";
+      name = "PltcalBlock";
       buildInputs = with pkgs; [
         nodejs_20
         yarn
